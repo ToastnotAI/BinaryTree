587c9034830fe72022a4e11bb803f86e928b1e6b
ToastnotAI <ryanchristie03@gmail.com>
2025-12-11 14:28:59 +0000

    Implement BinaryTree class with insert method and corresponding unit tests

--- BinaryTree.py
+++ BinaryTree.py
@@ -1,8 +1,15 @@
-class BinaryTree:
-    pass
-
 class Node:
     def __init__(self, value):
         self.value = value
         self.left = None
         self.right = None
+    
+    
+class BinaryTree:
+    root = None
+
+    def insert(self, new_node):
+        if self.root is None:
+            self.root = new_node
+        else:
+            self.root.left = new_node
--- BinaryTree_test.py
+++ BinaryTree_test.py
@@ -1,15 +1,6 @@
 import unittest
 from BinaryTree import *
 
-class TestBinaryTree(unittest.TestCase):
-
-    def test_import_binary_tree(self):
-        try:
-            from BinaryTree import BinaryTree
-        except ImportError as e:
-            self.fail(f"Importing BinaryTree failed with error: {e}")
-
-
 class TestBinaryTreeNode(unittest.TestCase):
 
     def test_import_node(self):
@@ -24,4 +15,35 @@ class TestBinaryTreeNode(unittest.TestCase):
         self.assertEqual(node.value, 10) 
         self.assertIsNone(node.left)
         self.assertIsNone(node.right)
+
+
+class TestBinaryTree(unittest.TestCase):
+
+    def test_import_binary_tree(self):
+        try:
+            from BinaryTree import BinaryTree
+        except ImportError as e:
+            self.fail(f"Importing BinaryTree failed with error: {e}")
+
+    def test_tree_initialization(self):
+        tree = BinaryTree()
+        self.assertIsNotNone(tree)
+        self.assertIsNone(tree.root)
+
+    def test_add_root_node(self):
+        tree = BinaryTree()
+        tree.insert(Node(20))
+        self.assertEqual(type(tree.root), Node)
+        self.assertEqual(tree.root.value, 20)
+
+    def test_add_single_left_child(self):
+        tree= BinaryTree()
+        tree.root = Node(30)
+        left_child = Node(15)
+        tree.insert(left_child)
+        self.assertEqual(tree.root.left, left_child)
+
+
+    
+
         
